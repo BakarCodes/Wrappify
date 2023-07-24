333f1c26ede2badaffcb891e4023cf89936425a5
Abubakar Gudal <86916278+BakarCodes@users.noreply.github.com>
2023-07-24 03:52:27 +0100

    wrapped

---
+++ wrappify/src/Components/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='Home'>
+        <section class="one">
+          <h1 className='title'>
+            BAKAR
+          </h1>
+          <h2 className='subheading'>
+            SOFTWARE DEVELOPER 
+          </h2>
+          <p className='location'>
+            LONDON
+          </p>
+        </section>
+    </div>
+  )
+}
+
+export default Home
